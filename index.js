a3e89396051e9c0bb4b7de3a551244ff02b88966
xrtyghuji7 <gs1luciasbd@hotmail.com>
2024-05-07 02:12:59 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-function longestPalindrome(s) {
-  let longest = "";
-  for (let i = 0; i < s.length; i++) {
-    const oddPalindrome = expandAroundCenter(s, i, i);
-    const evenPalindrome = expandAroundCenter(s, i, i + 1);
-    const currentLongest =
-      oddPalindrome.length > evenPalindrome.length
-        ? oddPalindrome
-        : evenPalindrome;
-    if (currentLongest.length > longest.length) longest = currentLongest;
+function exist(board, word) {
+  const rows = board.length;
+  const cols = board[0].length;
+  const dfs = (i, j, index) => {
+    if (index === word.length) return true;
+    if (i < 0 || i >= rows || j < 0 || j >= cols || board[i][j] !== word[index])
+      return false;
+    const temp = board[i][j];
+    board[i][j] = "";
+    const found =
+      dfs(i + 1, j, index + 1) ||
+      dfs(i - 1, j, index + 1) ||
+      dfs(i, j + 1, index + 1) ||
+      dfs(i, j - 1, index + 1);
+    board[i][j] = temp;
+    return found;
+  };
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (board[i][j] === word[0] && dfs(i, j, 0)) return true;
+    }
   }
-  return longest;
-}
-function expandAroundCenter(s, left, right) {
-  while (left >= 0 && right < s.length && s[left] === s[right]) {
-    left--;
-    right++;
-  }
-  return s.slice(left + 1, right);
+  return false;
 }
