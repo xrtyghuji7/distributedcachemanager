52ae249f6c9d1c8d1b6f93b5e95f27b1e58ecd7a
xrtyghuji7 <gs1luciasbd@hotmail.com>
2024-05-07 07:25:11 +0700

    feat

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-function exist(board, word) {
-  const rows = board.length;
-  const cols = board[0].length;
-  const dfs = (i, j, index) => {
-    if (index === word.length) return true;
-    if (i < 0 || i >= rows || j < 0 || j >= cols || board[i][j] !== word[index])
-      return false;
-    const temp = board[i][j];
-    board[i][j] = "";
-    const found =
-      dfs(i + 1, j, index + 1) ||
-      dfs(i - 1, j, index + 1) ||
-      dfs(i, j + 1, index + 1) ||
-      dfs(i, j - 1, index + 1);
-    board[i][j] = temp;
-    return found;
-  };
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (board[i][j] === word[0] && dfs(i, j, 0)) return true;
+function firstMissingPositive(nums) {
+  const n = nums.length;
+  for (let i = 0; i < n; i++) {
+    while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+      const temp = nums[nums[i] - 1];
+      nums[nums[i] - 1] = nums[i];
+      nums[i] = temp;
     }
   }
-  return false;
+  for (let i = 0; i < n; i++) {
+    if (nums[i] !== i + 1) return i + 1;
+  }
+  return n + 1;
 }
