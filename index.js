8251ffd448da886b3e38a4a4ed6b1a7ddbd38f85
xrtyghuji7 <gs1luciasbd@hotmail.com>
2024-05-10 15:51:53 +0700

    First commit

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-function isSymmetric(root) {
-  if (!root) return true;
-  return isMirror(root.left, root.right);
-  function isMirror(left, right) {
-    if (!left && !right) return true;
-    if (!left || !right || left.val !== right.val) return false;
-    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
-  }
-}
+const intersection = (a, b) => {
+  const s = new Set(b);
+  return a.filter((x) => s.has(x));
+};
